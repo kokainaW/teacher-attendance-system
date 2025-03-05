ddd8035581ae782c026834b25e278c17fea4e027
kokainaW <williamkhoko@yahoo.com>
2025-03-05 20:09:47 +0100

    supa

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,8 +3,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Load environment variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.https://oplfytljkygjcvaprlxf.supabase.co;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wbGZ5dGxqa3lnamN2YXBybHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMjU4NzYsImV4cCI6MjA1NjcwMTg3Nn0.utDJAdzfDqqKRpYyNQDhoP4uCxvJVG0lwBtUCUM1Zns;
 
 // Ensure the variables exist
 if (!supabaseUrl || !supabaseAnonKey) {
